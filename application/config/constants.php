64e3b9c7850e2c1e196752369544ff70a7344ff8
Michael Hendra Wijaya <michaelhendraw@gmail.com>
2019-01-29 21:09:19 +0700

    set constant dir

--- application/config/constants.php
+++ application/config/constants.php
@@ -88,5 +88,5 @@ defined('APP')      			OR define('APP', 'PKBM Berdaya Indonesia');
 defined('ADMIN')      			OR define('ADMIN', 'CMS '.APP);
 // defined('DIR')      			OR define('DIR', 'E:'.DIRECTORY_SEPARATOR.'xampp'.DIRECTORY_SEPARATOR.'htdocs'.DIRECTORY_SEPARATOR.'cms'.DIRECTORY_SEPARATOR);
 defined('DIR')      			OR define('DIR', DIRECTORY_SEPARATOR.'opt'.DIRECTORY_SEPARATOR.'lampp'.DIRECTORY_SEPARATOR.'htdocs'.DIRECTORY_SEPARATOR.'cms'.DIRECTORY_SEPARATOR);
-// defined('DIR')      			OR define('DIR', DIRECTORY_SEPARATOR.'public_html'.DIRECTORY_SEPARATOR.'cms'.DIRECTORY_SEPARATOR);
+defined('DIR')      			OR define('DIR', 'https://rogucms.herokuapp.com/uploads/');
 
