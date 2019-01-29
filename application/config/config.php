d93f5bc99bc214b6348f618a1787e571fc1eb64c
Michael Hendra Wijaya <michaelhendraw@gmail.com>
2019-01-29 21:00:29 +0700

    set base url and database

--- application/config/config.php
+++ application/config/config.php
@@ -26,6 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] = 'http://localhost/cms/';
 // $config['base_url'] = 'http://192.168.1.6/cms/';
 // $config['base_url'] = 'http://10.237.122.190/cms/';
+$config['base_url'] = 'https://rogucms.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
